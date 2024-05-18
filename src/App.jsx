f583fbe8db21179929755839058854210d7f1be2
Mutasim Ibrahim <mutasimibrahim01@gmail.com>
2024-05-18 13:54:07 -0500

    Completed styling for quote and source. Added json file and provided a selection of quotes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
+import React, { useState } from "react"
+import DhikrComponent from "./Components/DhikrComponent/DhikrComponent"
+import QuoteComponent from "./Components/QuoteComponent/QuoteComponent"
+
 export default function App() {
 
+  const [darkMode, setDarkMode] = useState()
+
   return (
-    <>
-      
-    </>
+  <div className="h-screen flex flex-row">
+    
+    <div className="QuoteSide w-2/3 bg-blue-600">
+      <DhikrComponent />
+    </div>
+
+    <div className="DhikrSide w-1/3 bg-red-600">
+      <QuoteComponent />
+    </div>
+
+  </div>
   )
 }
